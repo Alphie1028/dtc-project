d677e4a82609f2f06a10bb8357fd70a4d757f3c5
Alphie Brennan <alphie1028@gmail.com>
2025-02-23 17:14:17 -0800

    fix

--- server/index.js
+++ server/index.js
@@ -58,18 +58,22 @@ let generatedFiles = [];
 
 //Set up multer storage
 const storage = multer.diskStorage({
-  destination: (req, file, cb) =>{
-    cb(null, path.join(__dirname, '../client/src/assets'));
+  destination: (req, file, cb) => {
+    const uploadDir = path.join(__dirname, 'uploads');
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, uploadDir);
   },
-  filename: (req, file, cb) =>{
-    const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname);
-    cb(null, 'img-' + uniqueName + ext);
+  filename: (req, file, cb) => {
+    const uniqueName = `img-${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
+    cb(null, uniqueName);
   },
 });
 
 const upload = multer({ storage });
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 //Upload Route
 app.post('/upload', upload.array('images', 10), async (req, res) =>{
   try{
